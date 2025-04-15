84ad15e842cd268b086f281dc064a3fc28b16076
Gurkirpal Singh <12171804+gpalsingh@users.noreply.github.com>
2025-04-15 19:15:12 +0530

    feat: Include Antd notifications in global context

--- src/common/context/globals/globals-provider.tsx
+++ src/common/context/globals/globals-provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { createContext, ReactNode, useContext, useMemo } from "react";
-import useAuth from './useAuth';
+import useAuth from "./useAuth";
+import { notification } from "antd";
 
 type GlobalsContextType = Partial<{
   auth: ReturnType<typeof useAuth>;
@@ -11,15 +12,19 @@ const GlobalsContext = createContext<GlobalsContextType>({});
 
 export const GlobalsProvider = ({ children }: { children: ReactNode }) => {
   const authContext = useAuth();
+  const [notificationApi, notificationContextHolder] =
+    notification.useNotification();
 
   const contextValue = useMemo(() => {
     return {
       auth: authContext,
+      notificationApi,
     };
-  }, [authContext]);
+  }, [authContext, notificationApi]);
 
   return (
     <GlobalsContext.Provider value={contextValue}>
+      {notificationContextHolder}
       {children}
     </GlobalsContext.Provider>
   );
